6ebce5bc4052a04bc708300727e5a68e0e348bb1
SinhPN20796 <sinhpn20796@gmail.com>
2018-11-19 14:04:49 +0700

    19/10/2018

--- public/js/tree-init.js
+++ public/js/tree-init.js
@@ -5,7 +5,7 @@ var node_old_active = true;
 function valid_input() {
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
     var isError = false;
-	$(".error-msg").hide();
+	$('#add-employee-modal.error-msg').hide();
 	if ($('#id-employee-name-edit').val().trim() == '') {
 		$('#msg-name-up').show();
 		    isError = true;
@@ -134,7 +134,8 @@ function load_data_update(node){
     $('#msg-name-up').hide();
     $('#msg-empty-email-up').hide();
     $('#msg-invalid-email-up').hide();
-    $('.error-msg').hide();
+    $('#msg-invalid-position-edit').hide();
+    $('#add-employee-modal.error-msg').hide();
     if (node.data.active) {
     	$("#emp_active_1").click();
     } else {
